97d2593cdefa3eb3fee39114f4ccf691324e03d6
Arjun M <90825849+Arjun8420@users.noreply.github.com>
2021-10-03 19:56:10 +0530

    Add files via upload

---
+++ Area of circle using buildin function.py
@@ -0,0 +1,5 @@
+#Calculate the Area of Circle
+import math
+radius = int(input("Enter the radius = "))
+area = math.pi * radius **2
+print("Area of Circle = ",int(area))
---
+++ Area of circle.py
@@ -0,0 +1,4 @@
+#Program to find area of circle
+radius = int(input("Enter the radius = "))
+area = 3.14 * radius **2
+print("Area of Circle is = ",int(area))
